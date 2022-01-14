b8a70c61a0d103fac60bc2844c7b5d0ca45bddbb
Matthieu Rousseau <matthieurousseau86@gmail.com>
2022-01-14 12:54:15 +0400

    added /tweets route to API

--- app/apis/tweets.py
+++ app/apis/tweets.py
@@ -61,3 +61,7 @@ class TweetsResource(Resource):
             return tweet, 201
         else:
             return abort(422, "Tweet text can't be empty")
+
+    @api.marshal_with(json_tweet)
+    def get(self):
+        return db.session.query(Tweet).all(), 201
